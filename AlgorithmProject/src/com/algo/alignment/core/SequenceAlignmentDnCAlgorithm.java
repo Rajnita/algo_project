2b63b5a17a8be91db3b42cd24f608d3aeabeb7b8
KPherwani <kpherwan@usc.edu>
2021-12-08 20:00:31 -0800

    dnc change

--- AlgorithmProject/src/com/algo/alignment/core/SequenceAlignmentDnCAlgorithm.java
+++ AlgorithmProject/src/com/algo/alignment/core/SequenceAlignmentDnCAlgorithm.java
@@ -4,6 +4,7 @@ import com.algo.alignment.utils.UtilityFunctions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class SequenceAlignmentDnCAlgorithm {
@@ -11,7 +12,8 @@ public class SequenceAlignmentDnCAlgorithm {
     int globalCost = 0;
     public List<Integer> arrowPath = new ArrayList<>();
 
-    public void getAlignment(String input1, String input2) {
+
+    public int getAlignment(String input1, String input2) {
         int m = input1.length();
         int n = input2.length();
         int best = Integer.MAX_VALUE;
@@ -19,20 +21,20 @@ public class SequenceAlignmentDnCAlgorithm {
 
         if(m <= 2 || n <= 2) {
             globalCost += sequenceAlignmentBasicAlgorithm.alignSequences(input1, input2);
-            System.out.println(globalCost);
+            //System.out.println(globalCost);
         }
         else {
-            int v1, v2;
-            best = Integer.MAX_VALUE;
-            bestQ = 0;
-            for(int q = 1; q < n; q++) {
-                v1 = spaceEfficientAlignment(input1.substring(0, m/2), input2.substring(0, q));
-                v2 = spaceEfficientAlignment(input1.substring(m/2), input2.substring(q));
-                //System.out.println("sum " + (v1+v2));
-                //System.out.println("q " + q);
-                if((v1 + v2) < best) {
-                    bestQ = q;
-                    best = v1 + v2;
+            LinkedList<Integer> v1 = AllInput2PrefixCosts(input1.substring(0, m/2), input2);
+            System.out.println("v1 " + v1);
+            LinkedList<Integer> v2 = AllInput2SuffixCosts(input1.substring(m/2), input2);
+            //System.out.println("sum " + (v1+v2));
+            System.out.println("v2 " + v2);
+            //System.out.println("q " + q);
+            for(int q = 0; q < v1.size(); q++) {
+                if((v1.get(q) + v2.get(q)) < best) {
+                    bestQ = q+1;
+                    best = v1.get(q) + v2.get(q);
+                    System.out.println("sum " + best);
                 }
             }
 
@@ -43,13 +45,15 @@ public class SequenceAlignmentDnCAlgorithm {
             getAlignment(input1.substring(0, m/2), input2.substring(0, bestQ));
             getAlignment(input1.substring(m/2), input2.substring(bestQ));
         }
+        return globalCost;
     }
 
-    public int spaceEfficientAlignment(String input1, String input2) {
-
+    private LinkedList<Integer> AllInput2SuffixCosts(String input1, String input2) {
+        input1 = new StringBuffer(input1).reverse().toString();
         int m = input1.length();
         int n = input2.length();
 
+        LinkedList<Integer> result = new LinkedList<>();
         int[][] dp = new int[m+1][2];
 
         for(int i = 0; i <= m; i++) {
@@ -65,10 +69,53 @@ public class SequenceAlignmentDnCAlgorithm {
                 //System.out.print(dp[i][1] + " ");
             }
             //System.out.println();
+            System.out.print(Arrays.toString(dp[m]));
             for(int i = 0; i <= m; i++) {
                 dp[i][0] = dp[i][1];
             }
+
+            //result.addLast(dp[m][1]);
+            //System.out.print(dp[m][1] + " ");
+
         }
-        return dp[m][1];
+//        for(int i = 0; i <= m; i++) {
+//            result.addFirst(dp[i][1]);
+//            //System.out.print(dp[i][1] + " ");
+//        }
+        return result;
+    }
+
+    public LinkedList<Integer> AllInput2PrefixCosts(String input1, String input2) {
+        int m = input1.length();
+        int n = input2.length();
+
+        LinkedList<Integer> result = new LinkedList<>();
+        int[][] dp = new int[m+1][2];
+
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = i * 30;
+        }
+
+        for(int j = 1; j <= n; j++) {
+            dp[0][1] = j * 30;
+
+            for(int i = 1; i <= m; i++) {
+                dp[i][1] = Math.min(UtilityFunctions.getMismatchPenalty(input1.charAt(i-1), input2.charAt(j-1)) + dp[i-1][0], 30 + dp[i-1][1]);
+                dp[i][1] = Math.min(dp[i][1], 30 + dp[i][0]);
+                System.out.print(dp[i][1] + " ");
+            }
+            System.out.println();
+            for(int i = 0; i <= m; i++) {
+                dp[i][0] = dp[i][1];
+            }
+            result.addLast(dp[m][1]);
+            //System.out.print(dp[m][1] + " ");
+
+        }
+//        for(int i = 0; i <= m; i++) {
+//            result.addFirst(dp[i][1]);
+//            //System.out.print(dp[i][1] + " ");
+//        }
+        return result;
     }
 }
